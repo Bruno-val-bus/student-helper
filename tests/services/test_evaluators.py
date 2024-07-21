34b2eff6733da17e2ce7f3c8ed99dcaf7b9868b2
Bruno Alberto Valverde Bustamante <bruno.valverde.95@outlook.es>
2024-07-21 18:21:14 +0200

    separated grammatical evaluator tests into creation and content and added one parameter set

--- tests/services/test_evaluators.py
+++ tests/services/test_evaluators.py
@@ -1,4 +1,4 @@
-from typing import List, cast, Union
+from typing import List, Union
 from static.summary_metrics import RELEVANCE, COHERENCE, CONSISTENCY, FLUENCY
 import pytest
 
@@ -8,22 +8,7 @@ from services.error_finder_factory import TextEvaluatorFactory
 from services.error_finders import SummaryEvaluator, GrammaticalEvaluator, TextEvaluator
 from static.summary_example_text import afrikaans_OPENAI_summary_good, afrikaans_OPENAI_summary_bad
 
-
-@pytest.fixture
-def grammatical_evaluator() -> Union[GrammaticalEvaluator, TextEvaluator]:
-    factory = TextEvaluatorFactory(RecordingType.LANGUAGE_PRODUCTION)
-    grammatical_evaluator = factory.get_evaluator()
-    return grammatical_evaluator
-
-
-@pytest.fixture
-def summary_evaluator() -> Union[SummaryEvaluator, TextEvaluator]:
-    factory = TextEvaluatorFactory(RecordingType.COMPREHENSION)
-    summary_evaluator = factory.get_evaluator()
-    return summary_evaluator
-
-
-@pytest.mark.parametrize("sentence, error_item_reference", [
+param_sets = [
     ("After going to the store, she buyed some apples and oranges, but forgot to brings her wallet so she couldn't pays "
      "for them.", {
         "buyed": ErrorItem(error="buyed",
@@ -36,16 +21,73 @@ def summary_evaluator() -> Union[SummaryEvaluator, TextEvaluator]:
                           correction="pay",
                           category="Subject-Verb Agreement"),
         }
-     )
-])
-def test_grammatical_evaluate(grammatical_evaluator: GrammaticalEvaluator, sentence: str, error_item_reference: dict[str: ErrorItem]):
-    errors: Errors = grammatical_evaluator.evaluate(sentence)
-    error_items: List[ErrorItem] = errors.error
+     ),
+    ("The grass are green on the other side.", {
+        "are": ErrorItem(error="are",
+                         correction="is",
+                         category="Subject-Verb Agreement"),
+        }
+    )
+]
+
+
+@pytest.fixture(scope="session")
+def grammatical_evaluator() -> Union[GrammaticalEvaluator, TextEvaluator]:
+    factory = TextEvaluatorFactory(RecordingType.LANGUAGE_PRODUCTION)
+    grammatical_evaluator = factory.get_evaluator()
+    return grammatical_evaluator
+
+
+@pytest.fixture(scope="session")
+def error_items_list(grammatical_evaluator: GrammaticalEvaluator) -> List[List[ErrorItem]] :
+    error_items_list: List[List[ErrorItem]] = []
+    for parameter_set in param_sets:
+        sentence, _ = parameter_set
+        errors: Errors = grammatical_evaluator.evaluate(sentence)
+        error_items: List[ErrorItem] = errors.error
+        error_items_list.append(error_items)
+    return error_items_list
+
+
+@pytest.fixture(scope="session")
+def summary_evaluator() -> Union[SummaryEvaluator, TextEvaluator]:
+    factory = TextEvaluatorFactory(RecordingType.COMPREHENSION)
+    summary_evaluator = factory.get_evaluator()
+    return summary_evaluator
+
+
+@pytest.mark.parametrize("sentence, error_item_reference", param_sets)
+def test_grammatical_evaluate(error_items_list: List[List[ErrorItem]],
+                              request,
+                              sentence: str,
+                              error_item_reference: dict[str: ErrorItem]
+                              ):
+    """
+    Tests if ErrorItem attributes (category, error and correction) were found.
+    """
+    param_index = list(request.node.callspec.indices.values())[0]
+    error_items = error_items_list[param_index]
+    for error_item in error_items:
+        assert error_item.error is not None
+        assert error_item.category is not None
+        assert error_item.correction is not None
+
+
+@pytest.mark.parametrize("sentence, error_item_reference", param_sets)
+def test_grammatical_evaluate_content(error_items_list: List[List[ErrorItem]],
+                                      request,
+                                      sentence: str,
+                                      error_item_reference: dict[str: ErrorItem]
+                                      ):
+    """
+    Tests if content of found ErrorItem's (category, error and correction) match reference
+    ErrorItem's
+    """
+    param_index = list(request.node.callspec.indices.values())[0]
+    error_items = error_items_list[param_index]
     for error_item in error_items:
         ref_error_item: ErrorItem = error_item_reference.get(error_item.error, None)
         assert ref_error_item is not None
-        if ref_error_item is None:
-            continue
         assert error_item.category == ref_error_item.category
         assert error_item.correction == ref_error_item.correction
 
