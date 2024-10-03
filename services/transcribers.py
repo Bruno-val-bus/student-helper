584152d272aae2b888efaf9f5de6b86c89013f89
bruno <bruno.valverde.95@outlook.es>
2024-10-03 15:48:44 +0200

    corrected syntax for dictionary typing in transcribers

--- services/transcribers.py
+++ services/transcribers.py
@@ -4,17 +4,17 @@ from typing import Dict
 
 class ITranscription(ABC):
 
-    def transcribe(self, audio_file_path: str) -> Dict[str: float]:
+    def transcribe(self, audio_file_path: str) -> Dict[str, float]:
         ...
 
 
 class WhisperTranscription(ITranscription):
 
-    def transcribe(self, audio_file_path: str) -> Dict[str: float]:
+    def transcribe(self, audio_file_path: str) -> Dict[str, float]:
         pass
 
 
 class VulaVulaTranscription(ITranscription):
 
-    def transcribe(self, audio_file_path: str) -> Dict[str: float]:
+    def transcribe(self, audio_file_path: str) -> Dict[str, float]:
         pass
